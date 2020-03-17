c05fd3f18454e5ce962a8faadb0ada88709278c1
bukharov <dilmurod.bukharov@gmail.com>
2020-03-16 21:48:03 -0400

    added github and linkedin icons into my contact page

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,5 +1,11 @@
 import React from "react";
 import Layout from "../components/layout";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+library.add(fab, fas, far);
 const Contacts = () => (
   <Layout>
     <div
@@ -14,6 +20,30 @@ const Contacts = () => (
       <p>dilmurod.bukharov@gmail.com</p>
       <h3>Phone</h3>
       <p>Phone Number: (412)758-1167</p>
+      <div style={{ display: `flex`, flexDirection: `row` }}>
+        <div style={{ marginRight: `30px` }}>
+          <h4>GitHub</h4>
+          <a
+            href="https://github.com/dilmurodb"
+            target="_blank"
+            rel="noopener"
+            style={{ color: `black` }}
+          >
+            <FontAwesomeIcon icon={["fab", "github-square"]} size="3x" />
+          </a>
+        </div>
+        <div>
+          <h4>LinkedIn</h4>
+          <a
+            href="https://www.linkedin.com/in/dilmurod-bukharov-242987168/"
+            target="_blank"
+            rel="noopener"
+            style={{ color: `black` }}
+          >
+            <FontAwesomeIcon icon={["fab", "linkedin"]} size="3x" />
+          </a>
+        </div>
+      </div>
     </div>
   </Layout>
 );
