1d3f2e60a0de56e0ca6318f1924d23ff0777fe62
bukharov <dilmurod.bukharov@gmail.com>
2020-03-23 15:44:59 -0400

    added paragraphs to my about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,21 +6,32 @@ const AboutPage = () => (
     <div>
       <h1>About</h1>
       <p>
-        Hello, my name is Dilmurod Bukharov, I moved to the United States in the
-        Summer of 2018 and since then I fell in love with this beautiful country
-        and great people. I graduated{" "}
+        Hello, my name is Dilmurod Bukharov, I am a Software Engineer and a
+        Traveler. I moved to the United States in the Summer of 2018 and since
+        then I fell in love with this beautiful country and great people.
+      </p>
+      <p>
+        I graduated{" "}
         <a href="https://en.misis.ru/" target="_blank" rel="noopener">
           National University of Science and Technology "MISIS" (Moscow
           Institute of Steel and Alloys)
         </a>{" "}
         in Moscow city, Russia and have both Bachelor’s and Master’s degree in
-        metallurgical engineering. I am intellectually curious and love spending
-        my spare time surfing the Internet and learning new things. I was always
-        fascinated by web technologies and how it works but never really had a
-        chance to learn it thoroughly. After I moved to the United States, I
-        decided to change my career path and learn Software Development. I knew
-        that changing my career would be challenging but I also was sure that it
-        would be extremely rewarding. Finally, when I found out about{" "}
+        metallurgical engineering.
+      </p>
+      <p>
+        {" "}
+        I am intellectually curious and love spending my spare time surfing the
+        Internet and learning new things. I was always fascinated by web
+        technologies and how it works but never really had a chance to learn it
+        thoroughly. After I moved to the United States, I decided to change my
+        career path and learn Software Development. I knew that changing my
+        career would be challenging but I also was sure that it would be
+        extremely rewarding.
+      </p>
+      <p>
+        {" "}
+        Finally, when I found out about{" "}
         <a href="https://generalassemb.ly/" target="_blank" rel="noopener">
           General Assembly
         </a>
